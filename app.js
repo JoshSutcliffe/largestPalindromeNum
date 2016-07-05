58c706c798fa08e8acdeb8486ac81964ebc2a142
JoshSutcliffe <joshclement12@gmail.com>
2016-07-05 15:26:21 +0100

    found highest number, trying to print it

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 console.log("connected");
+
+var arr = [];
+
+var checkNum2 =function(num) {
+  for (var i = 100; i < num; i++) {
+    var score = i * num;
+    var string = score.toString();
+    var reversed = string.split("").reverse().join("");
+    debugger
+    if (string === reversed) {
+      arr.push(parseInt(reversed));
+    };
+    if (score === 99999) {
+      console.log(i, num);
+    }
+  };
+};
+
+var checkNum1 = function (num) {
+  for (var i = 100; i < num; i++) {
+    checkNum2(i);
+  };
+  console.log(arr.sort());
+};
+
+checkNum1(999);
